24e0a4b48f8aa3e53d6e2fa91177a57e6ea08df2
JeffersonSilva <drankhar@gmail.com>
2021-12-23 11:06:11 -0300

    commit inicial Projeto Criado

---
+++ cadastromania/src/main/java/br/com/cadastromania/CadastromaniaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastromania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastromaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastromaniaApplication.class, args);
+	}
+
+}
---
+++ cadastromania/src/test/java/br/com/cadastromania/CadastromaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastromania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastromaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
