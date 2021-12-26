73ed5df128ee486b80ebf6a7d063139b70ef7ed4
JeffersonSilva <drankhar@gmail.com>
2021-12-25 23:13:17 -0300

    adicionado comando de formatação sql no banco de dados e adicionamos as anotações para a criação da tabela categoria no banco de dados.

--- cadastromania/src/main/java/br/com/cadastromania/domain/Categoria.java
+++ cadastromania/src/main/java/br/com/cadastromania/domain/Categoria.java
@@ -3,9 +3,18 @@ package br.com.cadastromania.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// Cria a Tabela no banco de dados
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String nome;
 	
